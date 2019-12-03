0dd70b6f5387ac0d6ab8081f1570009589d0f9f0
KalinaMichal <michal230915@gmail.com>
2019-12-03 18:04:45 +0100

    zmienilem Server (naprawa kompletna)

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,10 +1,10 @@
-        package server;
+package server;
 
-        import java.io.IOException;
-        import java.io.ObjectInputStream;
-        import java.io.ObjectOutputStream;
-        import java.net.ServerSocket;
-        import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
@@ -13,7 +13,7 @@
  */
 
 
-        public class Server {
+public class Server {
         private static ServerSocket server;
         /*port socket serer-a*/
         private static int port = 6666;
@@ -21,15 +21,15 @@
 
 
 
-        public static void main() throws IOException, ClassNotFoundException, InterruptedException {
-        /*tworzenie socket serwer*/
-        server = new ServerSocket(port);
+        public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+                /*tworzenie socket serwer*/
+                server = new ServerSocket(port);
                 int a1=20;
                 int b1=20;
                 int a2=20;
                 int b2=20;
-        System.out.println("Stworzylem server");
-        /*Tutaj daje wiadomosc klientom ktory byl pierwszy*/
+                System.out.println("Stworzylem server");
+                /*Tutaj daje wiadomosc klientom ktory byl pierwszy*/
 
                 //******************************//
                 //---- UNDER CONSTRUCKTION------//
@@ -88,65 +88,63 @@
 
 
 
-        while(true){
-                /**
-                 *
-                 * Lacze sie z kientem nr 1
-                 *
-                 * */
+                while(true){
+                        /**
+                         *
+                         * Lacze sie z kientem nr 1
+                         *
+                         * */
 
-                /*Czekam na klienta 1*/
-                System.out.println("Czekam na 1 gracza");
-                Socket socket1 = server.accept();
-                System.out.println("Gracz 1 dolaczyl do serwera");
+                        /*Czekam na klienta 1*/
+                        System.out.println("Czekam na 1 gracza");
+                        Socket socket1 = server.accept();
+                        System.out.println("Gracz 1 dolaczyl do serwera");
 
-                /*Teraz biore wiadomosc od klienta 1 */
-                ObjectInputStream checker1 = new ObjectInputStream(socket1.getInputStream());
+                        /*Teraz biore wiadomosc od klienta 1 */
+                        ObjectInputStream checker1 = new ObjectInputStream(socket1.getInputStream());
 
-                /*Konwertuje na inta*/
-                a1 = (int) checker1.readObject();
-                System.out.println("Dostalem widomosc od 1 gracza: " + a1);
-                /*Konwertuje na inta*/
-                b1 = (int) checker1.readObject();
-                System.out.println("Dostalem widomosc od 1 gracza: " + b1);
-
-
-
-                /**
-                 *
-                 * Lacze sie z kientem nr 2
-                 *
-                 * */
-                /*Czekam na klienta 2*/
-                System.out.println("Czekam na 2 gracza");
-                Socket socket2 = server.accept();
-                System.out.println("Gracz 2 dolaczyl do serwera");
-
-                /*Teraz biore wiadomosc od klienta 2*/
-                ObjectInputStream checker2 = new ObjectInputStream(socket2.getInputStream());
-
-                /*Konwertuje na inta*/
-                a2 = (int) checker2.readObject();
-                System.out.println("Dostalem widomosc od 2 gracza: " + a2);
-                b2 = (int) checker2.readObject();
-                System.out.println("Dostalem widomosc od 2 gracza: " + b2);
-
-                /*Daje klientowi 1 odpowiedz*/
-                ObjectOutputStream oos1 = new ObjectOutputStream(socket1.getOutputStream());
-                oos1.writeObject(a2);
-                oos1.writeObject(b2);
-
-                /*Daje klientowi 2 odpowiedz*/
-                ObjectOutputStream oos2 = new ObjectOutputStream(socket2.getOutputStream());
-                oos2.writeObject(a1);
-                oos2.writeObject(b1);
+                        /*Konwertuje na inta*/
+                        a1 = (int) checker1.readObject();
+                        System.out.println("Dostalem widomosc od 1 gracza: " + a1);
+                        /*Konwertuje na inta*/
+                        b1 = (int) checker1.readObject();
+                        System.out.println("Dostalem widomosc od 1 gracza: " + b1);
 
 
 
-                /*Sprawdzam czy to juz koniec naszej zabawy*/
-                //if(message1.equalsIgnoreCase("exit")) break;
+                        /**
+                         *
+                         * Lacze sie z kientem nr 2
+                         *
+                         * */
+                        /*Czekam na klienta 2*/
+                        System.out.println("Czekam na 2 gracza");
+                        Socket socket2 = server.accept();
+                        System.out.println("Gracz 2 dolaczyl do serwera");
+
+                        /*Teraz biore wiadomosc od klienta 2*/
+                        ObjectInputStream checker2 = new ObjectInputStream(socket2.getInputStream());
+
+                        /*Konwertuje na inta*/
+                        a2 = (int) checker2.readObject();
+                        System.out.println("Dostalem widomosc od 2 gracza: " + a2);
+                        b2 = (int) checker2.readObject();
+                        System.out.println("Dostalem widomosc od 2 gracza: " + b2);
+
+                        /*Daje klientowi 1 odpowiedz*/
+                        ObjectOutputStream oos1 = new ObjectOutputStream(socket1.getOutputStream());
+                        oos1.writeObject(a2);
+                        oos1.writeObject(b2);
+
+                        /*Daje klientowi 2 odpowiedz*/
+                        ObjectOutputStream oos2 = new ObjectOutputStream(socket2.getOutputStream());
+                        oos2.writeObject(a1);
+                        oos2.writeObject(b1);
 
 
+
+                        /*Sprawdzam czy to juz koniec naszej zabawy*/
+                        //if(message1.equalsIgnoreCase("exit")) break;
 
 
 
@@ -166,25 +164,26 @@
 
 
 
-                /*Konce obcowanie z klientem drugim*/
-                /*zamykam wszystkie zrodla*/
-                checker1.close();
-                oos1.close();
-                socket1.close();
-                checker2.close();
-                oos2.close();
-                socket2.close();
-                /*Sprawdzam czy to juz koniec naszej zabawy*/
-               if(a2==-1) break;
+
+
+                        /*Konce obcowanie z klientem drugim*/
+                        /*zamykam wszystkie zrodla*/
+                        checker1.close();
+                        oos1.close();
+                        socket1.close();
+                        checker2.close();
+                        oos2.close();
+                        socket2.close();
+                        /*Sprawdzam czy to juz koniec naszej zabawy*/
+                        if(a2==-1) break;
 
 
 
-        }
-        System.out.println("Shutting down Socket server!!");
-        //zamknij ServerSocket object
-        server.close();
-        }
-
+                }
+                System.out.println("Shutting down Socket server!!");
+                //zamknij ServerSocket object
+                server.close();
         }
 
+}
 
