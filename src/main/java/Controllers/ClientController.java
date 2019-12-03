fffd256a358243619ee834d7f1f1d2e21436f794
sasuke5055 <maciek.lewandowicz@gmail.com>
2019-12-03 23:38:38 +0100

    Poprawiony bot

--- src/main/java/Controllers/ClientController.java
+++ src/main/java/Controllers/ClientController.java
@@ -210,7 +210,7 @@ public class ClientController {
             diffR = Math.sqrt(diffX * diffX + diffY * diffY);
 
             if (diffR > 15) {
-                //clicked out of any points range
+                /*clicked out of any points range*/
             } else if (checkers[a][b] == null) {
                 cleanAllreadyChecked();
                 addChecker(a, b);
@@ -224,7 +224,7 @@ public class ClientController {
                 yourTurn=false;
             }
             else yourTurn=true;//to je chyba nie potrzebne
-        }else          System.out.println("To nie twoja tura, poczekoj");
+        }else          System.out.println("To nie twoja tura, poczekaj");
 
     }
 
@@ -643,7 +643,7 @@ public class ClientController {
         return false;
     }
 
-    void botMove(){       //przykłądowy bot
+    void botMove(){       /*przykładowy bot*/
 
         int[][] killPotential = new int[19][19];
         for(int i=0;i<19;i++){
--- src/test/java/gameRulesTests/KillInCOrnerController.java
+++ src/test/java/gameRulesTests/KillInCOrnerController.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 /*Test bada lwy gorny rog planszy*/
 public class KillInCOrnerController extends GuiController{
+    private Ellipse[][] checkers = getCheckers();
+
 
     @Test
     public void test() {
@@ -20,12 +22,12 @@ public class KillInCOrnerController extends GuiController{
         Ellipse pc = new Ellipse();
         pc.setFill(Color.BLACK);
         pb.setFill(Color.WHITE);
-        test.checkers[0][1]=pb;
-        test.checkers[1][0]=pb;
+        checkers[0][1]=pb;
+        checkers[1][0]=pb;
         test.isBlack=true;
         /*nie korzystam z addChecker poniewaz jest tam czesc odpowiedzialna za dodanie do planszy*/
-        test.checkers[0][0]=pc;
-        assert(test.isSuicide(0,0));
+        checkers[0][0]=pc;
+        assert(test.isSuicide2(0,0));
 
     }
 
