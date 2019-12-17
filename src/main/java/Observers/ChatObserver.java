0d531bf1e87ca1555605e86c18fbcb3332cc32e2
KalinaMichal <michal230915@gmail.com>
2019-12-17 20:49:17 +0100

    Dodane kom. do javadoc

--- src/main/java/Observers/ChatObserver.java
+++ src/main/java/Observers/ChatObserver.java
@@ -117,7 +117,6 @@ public class ChatObserver extends Thread{
 
                 }
                 else if(mesIn.equals("Koniec gry!")){
-
                     chatController.setBtnContinueDisabled();
                 }
 
