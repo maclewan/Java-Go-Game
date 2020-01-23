2affb6fbb23576c9c14efe2ffa184ff6593240a4
KalinaMichal <michal230915@gmail.com>
2020-01-23 12:42:05 +0100

    Dodalem przykladowe polaczenie z baza

--- src/main/java/hibernate/TestHibernate.java
+++ src/main/java/hibernate/TestHibernate.java
@@ -1,4 +1,36 @@
 package hibernate;
 
+
+import hibernate.dto.GameStateEntity;
+
+import org.hibernate.Session;
+
 public class TestHibernate {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        /**
+         *
+         * MACIEJU,
+         *
+         * tutaj mamy przyklad dodawania czegos do
+         * bazy wystarczy to wpisac w odpowiednie miejsca
+         */
+
+        //Add new Employee object
+        GameStateEntity emp = new GameStateEntity();
+        emp.setGameId(0);
+        emp.setMoveId(0);
+        emp.setY(1);
+        emp.setX(2);
+        emp.setisBlack(true);
+
+        session.save(emp);
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+    }
+
 }
